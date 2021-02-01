91c217cc0a1433edb142ab18b2b6d5439e4abf79
Aleksandr Alekseev <hoblex@gmail.com>
2021-02-01 23:51:29 +0300

    исправлены замечания

--- js/main.js
+++ js/main.js
@@ -1,35 +1,9 @@
-let getRandomInteger = function (beginNumber, endNumber) {
-  if ((typeof beginNumber !== 'number') || typeof endNumber !== 'number') {
-    return 'Входные значения должны быть числами';
-  }
-
-  if (!Number.isInteger(beginNumber ) || !Number.isInteger(endNumber )) {
-    return 'Числа должны быть целыми';
-  }
-
-  if (beginNumber > endNumber) {
-    return 'Первый аргумент должен быть меньше или равен второму';
-  }
-
-  return  Math.round(Math.random() * (endNumber - beginNumber)) + beginNumber;
-}
+const getRandomInteger = (beginNumber, endNumber) => Math.round(Math.random() * (endNumber - beginNumber)) + beginNumber;
 
 getRandomInteger(1, 10);
 
-let getRandomFloat = function (beginNumber, endNumber, numbersAfterPoint) {
-  if ((typeof beginNumber !== 'number') || typeof endNumber !== 'number' || typeof numbersAfterPoint !== 'number') {
-    return 'Входные значения должны быть числами';
-  }
 
-  if (!Number.isInteger(numbersAfterPoint) || numbersAfterPoint < 0) {
-    return 'Количество знаков после запятой должно быть целым положительным числом';
-  }
+const getRandomFloat = (beginNumber, endNumber, numbersAfterPoint) => (Math.random() * (endNumber - beginNumber) + beginNumber).toFixed(numbersAfterPoint);
 
-  if (beginNumber > endNumber) {
-    return 'Первый аргумент должен быть меньше или равен второму';
-  }
+getRandomFloat(1.1, 1.2, 4);
 
-  return  (Math.random() * (endNumber - beginNumber) + beginNumber).toFixed(numbersAfterPoint);
-}
-
-getRandomFloat(1.1, 1.1, 3);
