b6b7e860b9fdf624430090a9c253c3741d966e52
Aleksandr Alekseev <hoblex@gmail.com>
2021-03-10 23:45:56 +0300

    созданы модули с данными и утилитарными функциями

---
+++ js/data.js
@@ -0,0 +1,42 @@
+export const IMG_ADDRESS = 'img/avatars/user';
+
+export const OBJECTS_COUNT = 10;
+export const TITLES = [
+  'У нас вы забудете обо всем на свете!',
+  'Отдохнете - мама родная не узнает!',
+  'Одиссей, тебе сюда от жены от детей!',
+  'Приезжай, торопись! Отдыхай - не скупись!',
+  'Отдохнете лучше, чем сын маминой подруги!',
+  'Клюет лучше, чем на черных камнях!',
+  'Приехал, заплатил, отдохнул!',
+  'Сколько волка не корми, а он все равно у нас отдыхать любит!',
+  'Семь раз отмерь, а отдохни у нас!',
+  'Хоромы царские, перины мягкие!',
+];
+
+export const TYPES = [ 'palace', 'flat', 'house', 'bungalow' ];
+
+export const CHECKIN_TIMES = ['12:00', '13:00', '14:00' ];
+
+export const CHECKOUT_TIMES = ['12:00', '13:00', '14:00' ];
+
+export const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner' ];
+
+export const DESCRIPTION = [
+  'Тепло, сухо, уютно',
+  'Красиво, уютно, чисто',
+  'Есть всё',
+  'Светло, тепло, уютно',
+  'Много пространства для отдыха',
+  'Есть вся необходимая техника',
+  'Скромно, но со вкусом',
+  'Окна с видом на море',
+  'Тихие соседи и их собаки',
+  'Уютное, теплое помещение. Почти как у Гарри',
+];
+
+export const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+]
--- js/main.js
+++ js/main.js
@@ -1,50 +1,18 @@
+import { IMG_ADDRESS } from './data.js';
+import { OBJECTS_COUNT } from './data.js';
+import { TITLES } from './data.js';
+import { TYPES } from './data.js';
+import { CHECKIN_TIMES } from './data.js';
+import { CHECKOUT_TIMES } from './data.js';
+import { FEATURES } from './data.js';
+import { DESCRIPTION } from './data.js';
+import { PHOTOS } from './data.js';
+
 const getRandomInteger = (beginNumber, endNumber) => Math.round(Math.random() * (endNumber - beginNumber)) + beginNumber;
 const getRandomFloat = (beginNumber, endNumber, numbersAfterPoint) => (Math.random() * (endNumber - beginNumber) + beginNumber).toFixed(numbersAfterPoint);
 
 getRandomInteger(1, 10);
 getRandomFloat(1.1, 1.2, 4);
-
-const IMG_ADDRESS = 'img/avatars/user';
-const OBJECTS_COUNT = 10;
-const TITLES = [
-  'У нас вы забудете обо всем на свете!',
-  'Отдохнете - мама родная не узнает!',
-  'Одиссей, тебе сюда от жены от детей!',
-  'Приезжай, торопись! Отдыхай - не скупись!',
-  'Отдохнете лучше, чем сын маминой подруги!',
-  'Клюет лучше, чем на черных камнях!',
-  'Приехал, заплатил, отдохнул!',
-  'Сколько волка не корми, а он все равно у нас отдыхать любит!',
-  'Семь раз отмерь, а отдохни у нас!',
-  'Хоромы царские, перины мягкие!',
-];
-
-const TYPES = [ 'palace', 'flat', 'house', 'bungalow' ];
-
-const CHECKIN_TIMES = ['12:00', '13:00', '14:00' ];
-
-const CHECKOUT_TIMES = ['12:00', '13:00', '14:00' ];
-
-const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner' ];
-
-const DESCRIPTION = [
-  'Тепло, сухо, уютно',
-  'Красиво, уютно, чисто',
-  'Есть всё',
-  'Светло, тепло, уютно',
-  'Много пространства для отдыха',
-  'Есть вся необходимая техника',
-  'Скромно, но со вкусом',
-  'Окна с видом на море',
-  'Тихие соседи и их собаки',
-  'Уютное, теплое помещение. Почти как у Гарри',
-];
-
-const PHOTOS = [
-  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-]
 
 const getRandomAvatar = (imgAddressArray) => {
   return {
