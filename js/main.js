566fd061c2eae7f6358ca2ce14ac2e1411084cad
Aleksandr Alekseev <hoblex@gmail.com>
2021-03-11 01:14:59 +0300

    для проверки eslint добалено использование const bookingObjectsList = new Array(OBJECTS_COUNT).fill(null).map(() => getNewBookingObject());

--- js/main.js
+++ js/main.js
@@ -16,8 +16,8 @@ const getNewBookingObject = () => {
 const OBJECTS_COUNT = 10;
 const bookingObjectsList = new Array(OBJECTS_COUNT).fill(null).map(() => getNewBookingObject());
 
-//Метод вызывается для того, чтобы ESLint не ругался
-// bookingObjectsList.forEach(value => {
-//   return value;
-// });
-console.log(bookingObjectsList);
+// Метод вызывается для того, чтобы ESLint не ругался
+bookingObjectsList.forEach(value => {
+  return value;
+});
+
