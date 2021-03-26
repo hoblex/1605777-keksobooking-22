92cdbfd9a77c746152c8f02c11339b9ec07474e2
Aleksandr Alekseev <hoblex@gmail.com>
2021-03-27 01:28:55 +0300

    исправлены синтаксические ошибки

--- js/util-functions.js
+++ js/util-functions.js
@@ -6,11 +6,11 @@ export const getRandomFloat = (beginNumber, endNumber, numbersAfterPoint) => (Ma
 
 export const showAlert = function () {
   const alertContainer = document.createElement('div');
-  alertContainer.style.zIndex = 1100;
+  alertContainer.style.zIndex = '1100';
   alertContainer.style.position = 'fixed';
-  alertContainer.style.left = 0;
-  alertContainer.style.top = 0;
-  alertContainer.style.right = 0;
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
   alertContainer.style.padding = '10px 3px';
   alertContainer.style.fontSize = '30px';
   alertContainer.style.textAlign = 'center';
